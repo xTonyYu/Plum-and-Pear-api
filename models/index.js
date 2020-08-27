7527a370effa2f392f4e4d97e39e7745af3bed4d
xTonyYu <xtonyyu@gmail.com>
2020-08-27 10:14:14 -0700

    deployed to Atlas

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/plumpear';
+const connectionString = process.env.MONGODB_ATLAS || 'mongodb://localhost:27017/plumpear';
 console.log('Mongo DB =', connectionString);
 
 const configOptions = {
--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const routes = require('./routes');
+const { resolve } = require("path");  // **** Stripe *****
 const port = process.env.PORT;
 const app = express();
+
+
 
 app.use(cors({
     methods: "GET,POST,PUT,DELETE",
@@ -15,6 +18,56 @@ app.use(cors({
 // middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+// **** Strip routes **********
+app.get("/", (req, res) => {
+  // Display checkout page
+    const path = resolve("./index.html");
+    res.sendFile(path);
+});
+
+const stripe = require('stripe')('pk_test_51HKd1qFxosFcUDRDfxXhRwFz4kNnu6schUawTEI4c3TOue7ezWNhB7NQ1fshiEIrBl2sc6CHLrhFr4T8XXjr475600fVxDizVg');
+
+// Endpoint for when `/pay` is called from client
+app.post('/pay', async (request, response) => {
+    try {
+        // Create the PaymentIntent
+        let intent = await stripe.paymentIntents.create({
+            amount: 0.25,
+            currency: 'usd',
+            payment_method: request.body.payment_method_id,
+
+            // A PaymentIntent can be confirmed some time after creation,
+            // but here we want to confirm (collect payment) immediately.
+            confirm: true,
+
+            // If the payment requires any follow-up actions from the
+            // customer, like two-factor authentication, Stripe will error
+            // and you will need to prompt them for a new payment method.>
+            error_on_requires_action: true
+        });
+        return generateResponse(response, intent);
+    } catch (e) {
+        if (e.type === 'StripeCardError') {
+            // Display error on client
+            return response.send({ error: e.message });
+        } else {
+            // Something else happened
+            return response.status(500).send({ error: e.type });
+        }
+    }
+});
+
+function generateResponse(response, intent) {
+    if (intent.status === 'succeeded') {
+        // Handle post-payment fulfillment
+        return response.send({ success: true });
+    } else {
+        // Any other status would be unexpected, so error
+        return response.status(500).send({error: 'Unexpected status ' + intent.status});
+    }
+}
+
 
 // routes to products and users
 app.use('/api/v1/products', routes.products);
